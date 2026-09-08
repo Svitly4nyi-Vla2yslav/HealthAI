690b92e013495879a0ad3288c0924eea5fa1779d
Vladyslav Svitlychnyi <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2026-09-08 09:28:05 +0200

    fix: correct welcome screen background color

--- app/index.tsx
+++ app/index.tsx
@@ -33,7 +33,7 @@ const WelcomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "fff",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
